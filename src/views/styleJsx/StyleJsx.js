4605df6cd879260ce523c82f1d5ebc57db6e1252
parade岁月 <46487578+parade0393@users.noreply.github.com>
2023-08-05 08:02:10 +0800

    事件传参

--- src/views/styleJsx/StyleJsx.js
+++ src/views/styleJsx/StyleJsx.js
@@ -8,14 +8,21 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    handleClick(event, id) {
+      console.log("handleClick:", event, id);
+    },
+  },
   render() {
-    const { title } = this;
+    const { title, handleClick } = this;
     return (
       <div
         class="style-jsx"
         style={{ color: "#ff0000", backgroundColor: "#eee", height: "100%" }}
       >
-        <p class="title">我是Hello World</p>
+        <p onClick={($event) => handleClick($event, 14)} class="title">
+          我是Hello World
+        </p>
         <p>{title || "子组件默认值"}</p>
       </div>
     );
